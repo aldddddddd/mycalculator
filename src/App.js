c31d1f07ce9fc7bef709344e7f564f1dca4ccec0
aldddddddd <ald.espanola@gmail.com>
2023-09-27 12:36:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+
+function CalcButton({ label }) {
+  return <button>{label}</button>;
+}
+
+function CalcDisplay({ display }) {
+  return (
+    <div className='CalcDisplay'>
+      {display}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='CalcContainer'>
+      <CalcDisplay display={"0"} />
+      <div className='ButtonContainer'>
+        <CalcButton label={7} />
+        <CalcButton label={8} />
+        <CalcButton label={9} />
+        <CalcButton label={"+"} />
+        <CalcButton label={4} />
+        <CalcButton label={5} />
+        <CalcButton label={6} />
+        <CalcButton label={"-"} />
+        <CalcButton label={1} />
+        <CalcButton label={2} />
+        <CalcButton label={3} />
+        <CalcButton label={"*"} />
+        <CalcButton label={"C"} />
+        <CalcButton label={"0"} />
+        <CalcButton label={"="} />
+        <CalcButton label={"/"} />
+      </div>
+
+      <button1><bold>Española</bold></button1>
+
     </div>
+
   );
 }
 
