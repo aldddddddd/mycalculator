292c6081ef9c21b36acfaa3711b2c72c95277363
aldddddddd <ald.espanola@gmail.com>
2023-10-05 19:35:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,61 +2,61 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [display, setDisplay] = useState('0');
-  const [equation, setEquation] = useState('');
-  const [fullname, setFullname] = useState('');
-  
-  const handleButtonClick = (buttonValue) => {
-    if (buttonValue === '=') {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
       try {
-        const result = eval(equation);
-        setDisplay(result.toString());
-        setEquation('');
+        setResult(eval(input).toString());
       } catch (error) {
-        setDisplay('Error');
-        setEquation('');
+        setResult('Error');
       }
-    } else if (buttonValue === 'C') {
-      setDisplay('0');
-      setEquation('');
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
     } else {
-      setDisplay((prevDisplay) =>
-        prevDisplay === '0' ? buttonValue : prevDisplay + buttonValue
-      );
-      setEquation((prevEquation) => prevEquation + buttonValue);
+      setInput(input + value);
     }
   };
 
-  const handleSurnameClick = () => {
-    setDisplay('Aldwin S. Española'); 
-  };
-  
-  return (
+  const handleSurnameButtonClick = () => {
     
-    <div className='CalcContainer'>
-      <h3>Calculator of Aldwin S. Española - IT3A</h3>
-      <div className='CalcDisplay'>{display}</div>
-      <div className='ButtonContainer'>
-        <button  onClick={() => handleButtonClick('7')}>7</button>
-        <button onClick={() => handleButtonClick('8')}>8</button>
-        <button onClick={() => handleButtonClick('9')}>9</button>
-        <button className='button-plus'onClick={() => handleButtonClick('+')}>+</button>
-        <button onClick={() => handleButtonClick('4')}>4</button>
-        <button onClick={() => handleButtonClick('5')}>5</button>
-        <button onClick={() => handleButtonClick('6')}>6</button>
-        <button className='button-minus'onClick={() => handleButtonClick('-')}>-</button>
-        <button onClick={() => handleButtonClick('1')}>1</button>
-        <button onClick={() => handleButtonClick('2')}>2</button>
-        <button onClick={() => handleButtonClick('3')}>3</button>
-        <button className='button-multiply'onClick={() => handleButtonClick('*')}>*</button>
-        <button className='button-clear'onClick={() => handleButtonClick('C')}>C</button>
-        <button onClick={() => handleButtonClick('0')}>0</button>
-        <button className='button-equal'onClick={() => handleButtonClick('=')}>=</button>
-        <button className='button-divide'onClick={() => handleButtonClick('/')}>&#247;</button>
-      </div>
-      <div className='SurnameButtonContainer'>
-        <button onClick={handleSurnameClick}>ESPAÑOLA</button>
+    setInput('ESPAÑOLA');
+  };
 
+  return (
+    <div className="calculator">
+      <h3>Calculator of Española, Aldwin, S. -IT3A</h3>
+      <div className="display">
+        <div className="output">{result || input || '0'}</div>
+        <div className="input">{input || '0'}</div>
+      </div>
+      <div className="container-button">
+        <div className="button">
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button className='button-plus' onClick={() => handleButtonClick('+')} >+</button>
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button className='button-minus' onClick={() => handleButtonClick('-')} >-</button>
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button className='button-multiply' onClick={() => handleButtonClick('*')} >*</button>
+          <button className='button-clear' onClick={() => handleButtonClick('C')}>C</button>
+          <button onClick={() => handleButtonClick('0')} >0</button>
+          <button className='button-equal' onClick={() => handleButtonClick('=')} >=</button>
+          <button className='button-divide' onClick={() => handleButtonClick('/')} >÷</button>
+        </div>
+      </div>
+
+      <div className="SurnameButtonContainer">
+        <div className="SurnameButton">
+          <button onClick={() => handleSurnameButtonClick('ESPAÑOLA')}>ESPAÑOLA</button>
+        </div>
       </div>
     </div>
   );
