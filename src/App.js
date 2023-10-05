a843cac165a3fcfce26e98e39e8b885a182610e6
aldddddddd <ald.espanola@gmail.com>
2023-10-05 09:56:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function CalcButton({ label }) {
-  return <button>{label}</button>;
-}
-
-function CalcDisplay({ display }) {
-  return (
-    <div className='CalcDisplay'>
-      {display}
-    </div>
-  );
-}
-
 function App() {
+  const [display, setDisplay] = useState('0');
+  const [equation, setEquation] = useState('');
+  const [fullname, setFullname] = useState('');
+  
+  const handleButtonClick = (buttonValue) => {
+    if (buttonValue === '=') {
+      try {
+        const result = eval(equation);
+        setDisplay(result.toString());
+        setEquation('');
+      } catch (error) {
+        setDisplay('Error');
+        setEquation('');
+      }
+    } else if (buttonValue === 'C') {
+      setDisplay('0');
+      setEquation('');
+    } else {
+      setDisplay((prevDisplay) =>
+        prevDisplay === '0' ? buttonValue : prevDisplay + buttonValue
+      );
+      setEquation((prevEquation) => prevEquation + buttonValue);
+    }
+  };
+
+  const handleSurnameClick = () => {
+    setDisplay('Aldwin S. Española'); 
+  };
+  
   return (
+    
     <div className='CalcContainer'>
-      <CalcDisplay display={"0"} />
+      <h3>Calculator of Aldwin S. Española - IT3A</h3>
+      <div className='CalcDisplay'>{display}</div>
       <div className='ButtonContainer'>
-        <CalcButton label={7} />
-        <CalcButton label={8} />
-        <CalcButton label={9} />
-        <CalcButton label={"+"} />
-        <CalcButton label={4} />
-        <CalcButton label={5} />
-        <CalcButton label={6} />
-        <CalcButton label={"-"} />
-        <CalcButton label={1} />
-        <CalcButton label={2} />
-        <CalcButton label={3} />
-        <CalcButton label={"*"} />
-        <CalcButton label={"C"} />
-        <CalcButton label={"0"} />
-        <CalcButton label={"="} />
-        <CalcButton label={"/"} />
+        <button  onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button className='button-plus'onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button className='button-minus'onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button className='button-multiply'onClick={() => handleButtonClick('*')}>*</button>
+        <button className='button-clear'onClick={() => handleButtonClick('C')}>C</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button className='button-equal'onClick={() => handleButtonClick('=')}>=</button>
+        <button className='button-divide'onClick={() => handleButtonClick('/')}>&#247;</button>
       </div>
+      <div className='SurnameButtonContainer'>
+        <button onClick={handleSurnameClick}>ESPAÑOLA</button>
 
-      <button1><bold>Española</bold></button1>
-
+      </div>
     </div>
-
   );
 }
 
